f15dba3a792ec2478c7bda46d430f78cc95903fe
Rik Alblas <rikalblas@gmail.com>
2024-10-16 16:13:45 +0200

    hoofdstuk 11

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+from flask import Flask
+
 app=Flask(__name__)
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run(port=5000,debug=True)
 
-    @app.route('/')
-    def home():
-        return "<p>Hello World!<p>"
+@app.route('/')
+def home():
+    return "Hoofdstuk 11"
+    
